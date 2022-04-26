a699a8cef4e85cbe2f362dda1d50b406125f5a66
hollownicky <70936757+hollownicky@users.noreply.github.com>
2022-04-26 10:29:39 +0800

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,76 @@
+import { SettingDrawer } from '@ant-design/pro-layout';
+import { PageLoading } from '@ant-design/pro-layout';
+import { history } from 'umi';
+import RightContent from '@/components/RightContent';
+import Footer from '@/components/Footer';
+import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import defaultSettings from '../config/defaultSettings';
+const loginPath = '/user/login';
+
+export const initialStateConfig = {
+  loading: <PageLoading />,
+};
+
+export async function getInitialState() {
+  const fetchUserInfo = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      return token;
+    } catch (error) {
+      history.push(loginPath);
+    }
+    return undefined;
+  };
+
+  // 如果不是登录页面，执行
+  if (history.location.pathname !== loginPath) {
+    const currentUser = await fetchUserInfo();
+    return {
+      fetchUserInfo,
+      currentUser,
+      settings: defaultSettings,
+    };
+  }
+
+  return {
+    fetchUserInfo,
+    settings: defaultSettings,
+  };
+}
+
+export const layout = ({ initialState, setInitialState }) => {
+  return {
+    rightContentRender: () => <RightContent />,
+    disableContentMargin: false,
+    waterMarkProps: {
+      content: initialState?.currentUser?.name,
+    },
+    footerRender: () => <Footer />,
+    onPageChange: () => {
+      // 如果没有登录，重定向到 login
+      // const { location } = history;
+      // if (!initialState?.currentUser && location.pathname !== loginPath) {
+      //   history.push(loginPath);
+      // }
+    },
+    menuHeaderRender: undefined,
+    childrenRender: (children, props) => {
+      return (
+        <>
+          {children}
+          {!props.location?.pathname?.includes('/login') && (
+            <SettingDrawer
+              disableUrlParams
+              enableDarkTheme
+              settings={initialState?.settings}
+              onSettingChange={(settings) => {
+                setInitialState((preInitialState) => ({ ...preInitialState, settings }));
+              }}
+            />
+          )}
+        </>
+      );
+    },
+    ...initialState?.settings,
+  };
+};
